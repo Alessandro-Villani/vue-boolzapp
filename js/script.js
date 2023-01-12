dde0e0671d811fd1c4e64a32edb459dd670f1b62
Alessandro-Villani <alessvill@libero.it>
2023-01-12 21:59:37 +0100

    added send and receive messages

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@ const app = Vue.createApp({
     data(){
         return{
             data,
-            currentIndex: 0
+            currentIndex: 0,
+            newMessage: ''
         }
     },
     methods:{
@@ -14,6 +15,30 @@ const app = Vue.createApp({
         },
         isSent(i){
             return this.data.contacts[this.currentIndex].messages[i].status === 'sent'
+        },
+        sendMessage(){
+            console.log('enter');
+            const message = {
+                date: new Date().toLocaleString(),
+                text: this.newMessage,
+                status: 'sent'
+            }
+            this.data.contacts[this.currentIndex].messages.push(message);
+            this.receiveMessage();
+            this.clearMessageField();
+        },
+        receiveMessage(){
+            setTimeout(() => {
+                const message = {
+                    date: new Date().toLocaleString(),
+                    text: 'ok',
+                    status: 'received'
+                }
+                this.data.contacts[this.currentIndex].messages.push(message);
+            }, 1000);
+        },
+        clearMessageField(){
+            this.newMessage = '';
         }
     }
 });
