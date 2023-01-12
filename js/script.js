da808a31b17959433f172537ac4dbb141ddc874c
Alessandro-Villani <alessvill@libero.it>
2023-01-12 19:15:34 +0100

    added chat switch

--- js/script.js
+++ js/script.js
@@ -3,8 +3,17 @@ console.log('VUE OK', Vue);
 const app = Vue.createApp({
     data(){
         return{
-            data
-            
+            data,
+            currentIndex: 0
+        }
+    },
+    methods:{
+        selectContact(i){
+            console.log('click')
+            this.currentIndex = i;
+        },
+        isSent(i){
+            return this.data.contacts[this.currentIndex].messages[i].status === 'sent'
         }
     }
 });
